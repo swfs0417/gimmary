f2be4584ffe69422b91aab9935586632781f89ee
swfs0417 <apdl0012@gmail.com>
2026-02-22 00:20:56 +0900

    feat(api): include groups router in the main API router

--- gimmary/api.py
+++ gimmary/api.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter
 from gimmary.app.users.router import user_router
 from gimmary.app.team.router import team_router
 from gimmary.app.missions.router import router as missions_router
+from gimmary.app.groups.router import groups_router
 
 api_router = APIRouter()
 
 api_router.include_router(user_router)
 api_router.include_router(team_router)
 api_router.include_router(missions_router)
+api_router.include_router(groups_router)
 # router.include_router()
