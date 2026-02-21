9f970ef0378dca7ee581aef39432737d005fe830
swfs0417 <apdl0012@gmail.com>
2026-02-22 03:52:18 +0900

    add cors

--- gimmary/main.py
+++ gimmary/main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.exception_handlers import request_validation_exception_handler
 from fastapi.exceptions import RequestValidationError
+from starlette.middeleware.cors import CORSMiddleware
 
 from gimmary.api import api_router
 
 app = FastAPI()
 
 app.include_router(api_router, prefix="/api")
+app.add_middleware(CORSMiddleware, allow_origins=["https://wafhk26-web.vercel.app/"], allow_methods=["*"], allow_headers=["*"])
+
 
 @app.get('/health')
 def health():
