9c52cb3e85f8379a3e4c1565a5962fc222508c54
swfs0417 <apdl0012@gmail.com>
2026-02-22 03:56:48 +0900

    1

--- gimmary/main.py
+++ gimmary/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.exception_handlers import request_validation_exception_handler
 from fastapi.exceptions import RequestValidationError
-from fastapi.middeleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from gimmary.api import api_router
 
